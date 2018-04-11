375aac6f3ccf77be055980846daf7e2aaf9fd546
Ben Halverson <benhalverson33@gmail.com>
2018-04-10 23:56:51 -0700

    added brick settings

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
-
+const ballRadius = 10;
+const paddleHeight = 10;
+const paddleWidth = 75;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = 2;
-const ballRadius = 10;
-const paddleHeight = 10;
-const paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
+
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
 
 function drawBall() {
   ctx.beginPath();
