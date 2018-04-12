c21768c33c099c701cba9e635ec6a1cf044db0e3
Ben Halverson <benhalverson33@gmail.com>
2018-04-11 22:16:38 -0700

    added mouse control for the paddle

--- src/index.js
+++ src/index.js
@@ -139,6 +139,13 @@ function keyUpHandler(e) {
   }
 }
 
+function mouseMoveHandler(e) {
+  const relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - (paddleWidth / 2);
+  }
+}
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
+document.addEventListener('mousemove', mouseMoveHandler, false);
 setInterval(draw, 10);
